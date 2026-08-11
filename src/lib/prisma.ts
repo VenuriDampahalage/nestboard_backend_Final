2aa79a77cb10911713ff9c1db25118d116e38571
VenuriDampahalage <dampahalagevenuri@gmail.com>
2026-08-11 13:58:39 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,19 @@
+// bridge --> helps Prisma talk to a PostgreSQL database
 import { PrismaPg } from "@prisma/adapter-pg";
+// generated specifically for database schema
 import { PrismaClient } from "../generated/client.js";
 import { env } from "./env.js";
 
-// global pattern
+// global singleton pattern
+//safe space in global environment to store database connection
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
+// Set up connection bridge using DATABASE_URL from environment variables
 export const adapter = new PrismaPg({
   connectionString: env.DATABASE_URL,
 });
 
+// Create actual database connection (prisma variable)
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
